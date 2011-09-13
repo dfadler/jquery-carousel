9204097f618177a3f3e115ee4e4936545e7614d0
Dustin <dustin@toky.com>
2011-09-13 17:10:47 -0500

    Building out

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,12 +1,10 @@
 $(function () {
 	$('#content-1')
 		.carousel({
-			controls: [true, true],
-			autoplay: 0
+			controls: [true, true]
 		});
 	$('#content-2')
 		.carousel({
-		controls: false,
-		autoplay: 0
+
 		});
 });
